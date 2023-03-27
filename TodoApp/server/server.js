dc4457525fb112b94606f18aca2663a7799d8d62
EmilHarutyunyan <emil.h098@gmail.com>
2023-03-27 17:01:23 +0400

    put and delete

--- TodoApp/server/server.js
+++ TodoApp/server/server.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Get
 app.get("/todos", (req, res) => {
   fs.promises.readFile(path.resolve("todo.json")).then((todos) => {
     res.send(todos);
   });
 });
 
+// Save
 app.post("/todos", (req, res) => {
   fs.promises
     .writeFile(
@@ -24,4 +26,35 @@ app.post("/todos", (req, res) => {
     });
 });
 
+// Edits
+app.put("/todos", (req, res) => {
+  const todo = req.body
+  const todos = fs.readFileSync(path.resolve("todo.json"));
+  const fileTodos = JSON.parse(todos);
+  const editTodos = fileTodos.map((item) => {
+    if (item.id === todo.id) {
+      return todo;
+    }
+    return item;
+  });
+  fs.writeFileSync(
+    path.resolve("todo.json"),
+    JSON.stringify(editTodos, undefined, 2)
+  )
+  res.json(editTodos);
+    
+});
+
+// Delete
+app.delete("/todos", (req, res) => {
+  const todo = req.body;
+  const todos = fs.readFileSync(path.resolve("todo.json"));
+  const fileTodos = JSON.parse(todos);
+  const editTodos = fileTodos.filter((item) => item.id !== todo.id);
+  fs.writeFileSync(
+    path.resolve("todo.json"),
+    JSON.stringify(editTodos, undefined, 2)
+  );
+  res.json(editTodos);
+});
 app.listen(3001);
